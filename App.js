09e8480e3f891f1b5d5a0449b588df82b56e6d38
DILIP SARVAIYA <dilipsarvaiya992@gmail.com>
2023-10-26 20:08:52 +0530

    Ignitin our app

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "i am an h1 tag"),
@@ -8,6 +10,7 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "i am an h2 tag"),
   ]),
 ]);
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
